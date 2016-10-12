348163b979a7511239b38358774b2675c1f5e6b8
Albert Chang <aznachang@gmail.com>
2016-10-11 20:13:56 -0700

    done with this

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,40 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //Recursive Array result
+  var result = []; 
+
+  //ARRAY CASE
+  if (Array.isArray(obj) ){
+
+    for (var i = 0; i < obj.length; i++){
+      //UNDEFINED OR FUNCTION CASE
+      if (obj[i] === undefined || typeof obj[i] === 'function'){
+        continue;
+      }
+      result.push( stringifyJSON(obj[i]) );
+    }
+    return '[' + result + ']';
+  }
+
+  //OBJECT CASE
+  else if (typeof obj === 'object' && obj){
+
+    for (var key in obj){
+      //UNDEFINED OR FUNCTION CASE
+      if (obj[key] === undefined || typeof obj[key] === 'function'){
+        continue;
+      }
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]) );
+    }
+    return '{' + result + '}';
+  }
+
+  //STRING CASE
+  else if ( typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+
+  return '' + obj;
 };
